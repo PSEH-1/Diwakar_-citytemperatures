ae9e77e32b4fa910d77005add39a221febcee9e2
PSEH-9 <PSEH-9@sapient.com>
2019-11-19 17:06:12 +0530

    test

--- README.md
+++
@@ -1,2 +0,0 @@
-# weather-next3days
-This is a test repository of getting weather for next 3 days
--- src/main/java/com/sapient/weatherapp/service/WeatherService.java
+++
@@ -1,7 +0,0 @@
-package com.sapient.weatherapp.service;
-
-import com.sapient.weather.entity.Weather;
-
-public interface WeatherService {
-	public Weather getWeatherForCity(String city);
-}
